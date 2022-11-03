33ded6f2cf234d36b5f700e919948ec1b21915ee
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-11-03 19:01:28 +0100

    fix: advancedfeatures

--- advancedfeatures/main.go
+++ advancedfeatures/main.go
@@ -1,33 +1,45 @@
 package main
 
 import (
-	"advancedfeatures/app"
 	"fmt"
 	"strings"
 )
 
+type HangManData struct {
+	Try             string
+	Letter          string
+	Randomword      string
+	TotalTries      int
+	NFormula        int
+	Slice           []string
+	SliceRandomword []string
+	SliceTry        []string
+	Boolean         bool
+	Boolean2        bool
+}
+
 func main() {
-	data := &app.HangManData{}
-	data.Randomword = strings.ToUpper(app.Randomword())
+	data := &HangManData{}
+	data.Randomword = strings.ToUpper(Randomword())
 	data.TotalTries = 10
 	data.NFormula = len(data.Randomword)/2 - 1
 	data.Slice = make([]string, len(data.Randomword))
 	data.SliceRandomword = make([]string, len(data.Randomword))
-	app.PrintLettersInTheFullSlice(data)
-	app.Start(data)
-	app.PrintNLetters(data)
+	PrintLettersInTheFullSlice(data)
+	Start(data)
+	PrintNLetters(data)
 	for true {
 		data.Boolean = false
 		data.Boolean2 = false
-		if app.IfZeroTry(data) == true {
+		if IfZeroTry(data) == true {
 			return
 		}
 		fmt.Print("\nChoose: ")
 		fmt.Scan(&data.Try)
 		data.Try = strings.ToUpper(data.Try)
-		app.IfInputIsTheFullWord(data)
-		app.IfInputIsTrue(data)
-		if app.IfSliceIsFull(data) == true {
+		IfInputIsTheFullWord(data)
+		IfInputIsTrue(data)
+		if IfSliceIsFull(data) == true {
 			fmt.Println("\n\nWell played!")
 			return
 		}
