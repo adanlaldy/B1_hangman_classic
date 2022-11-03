6dc9484e56952400019f73bd4abdcc23b66a609d
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-11-03 19:06:48 +0100

    fix: asciiart

--- asciiart/main.go
+++ asciiart/main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
-	"os"
+	"fmt"
 	"strings"
 )
 
-func PrintAscii(txt string) {
-	content, err := os.ReadFile("standard.txt")
-	if err != nil {
-		os.Exit(1)
-	}
-	split := strings.Split(string(content), "\n")
-	for d := 0; d < 9; d++ {
-		for index := 0; index < len(txt); index++ {
-			print(split[(int(txt[index]-32)*9)+d] + "")
-		}
-		println()
-	}
+type HangManData struct {
+	Try             string
+	Letter          string
+	Randomword      string
+	TotalTries      int
+	NFormula        int
+	Slice           []string
+	SliceRandomword []string
+	Boolean         bool
 }
+
 func main() {
-	PrintAscii("hello")
+	data := &HangManData{}
+	data.Randomword = Randomword()
+	data.TotalTries = 10
+	data.NFormula = len(data.Randomword)/2 - 1
+	data.Slice = make([]string, len(data.Randomword))
+	data.SliceRandomword = make([]string, len(data.Randomword))
+	PrintLettersInTheFullSlice(data)
+	Start(data)
+	PrintNLetters(data)
+	for true {
+		data.Boolean = false
+		if IfZeroTry(data.TotalTries, data.Randomword) == true {
+			return
+		}
+		fmt.Print("\nChoose: ")
+		fmt.Scan(&data.Try)
+		data.Try = strings.ToLower(data.Try)
+		IfInputIsTheFullWord(data)
+		IfInputIsTrue(data)
+		if IfSliceIsFull(data) == true {
+			fmt.Println("\n\nWell played!")
+
+			return
+		} else {
+			res := ""
+			for _, word := range data.Slice {
+				res = res + word
+			}
+			PrintAscii(res)
+		}
+	}
 }
