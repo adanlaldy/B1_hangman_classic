49af1dd1aef5aef3d836a30b268c3c950d178e8a
Adan LALDY-MAQUIHA <adan.laldymaquiha@ynov.com>
2022-11-03 19:08:13 +0100

    fix: hangmanclassic

--- hangmanclassic/main.go
+++ hangmanclassic/main.go
@@ -2,31 +2,41 @@ package main
 
 import (
 	"fmt"
-	"hangmanclassic/app"
 	"strings"
 )
 
+type HangManData struct {
+	Try             string
+	Letter          string
+	Randomword      string
+	TotalTries      int
+	NFormula        int
+	Slice           []string
+	SliceRandomword []string
+	Boolean         bool
+}
+
 func main() {
-	data := &app.HangManData{}
-	data.Randomword = strings.ToUpper(app.Randomword())
+	data := &HangManData{}
+	data.Randomword = strings.ToUpper(Randomword())
 	data.TotalTries = 10
 	data.NFormula = len(data.Randomword)/2 - 1
 	data.Slice = make([]string, len(data.Randomword))
 	data.SliceRandomword = make([]string, len(data.Randomword))
-	app.PrintLettersInTheFullSlice(data)
-	app.Start(data)
-	app.PrintNLetters(data)
+	PrintLettersInTheFullSlice(data)
+	Start(data)
+	PrintNLetters(data)
 	for true {
 		data.Boolean = false
-		if app.IfZeroTry(data) == true {
+		if IfZeroTry(data) == true {
 			return
 		}
 		fmt.Print("\nChoose: ")
 		fmt.Scan(&data.Try)
 		data.Try = strings.ToUpper(data.Try)
-		app.IfInputIsTheFullWord(data)
-		app.IfInputIsTrue(data)
-		if app.IfSliceIsFull(data) == true {
+		IfInputIsTheFullWord(data)
+		IfInputIsTrue(data)
+		if IfSliceIsFull(data) == true {
 			fmt.Println("\n\nWell played!")
 			return
 		}
